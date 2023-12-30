9f989329781ff56c4e953055b38ff67e8879d1da
gunjan <gunjan.soral@gmail.com>
2023-12-30 16:50:14 +0530

    hghghghtutututriririr

--- README.old.md
+++
@@ -1,1 +0,0 @@
-# sort-my-college-admin-panel
--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Dashboard />} />
+            <Route path="/users" element={<Users />} />
+          </>
+        ) : (
+          <>
+            <Route path="*" element={<Navigate replace to="/login" />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Login />} />
+            <Route path="/password-reset" element={<Login />} />
+          </>
+        )}
+      </Routes>
     </div>
   );
 }
