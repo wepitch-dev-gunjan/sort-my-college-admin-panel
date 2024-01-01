f2058555abd7df24a30b094804da8d82097c9876
Nmn200 <n9462895233@gmail.com>
2024-01-01 17:20:47 +0530

    qweqwertrt

--- src/pages/counsellor/index.jsx
+++ src/pages/counsellor/index.jsx
@@ -6,71 +6,94 @@ import { useState } from 'react';
 const User = () => {
   const [counsellors, setCounsellors] = useState([
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
-      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
-    },{
-      
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
-      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
-    },{
-      
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
+      status: 'Pending',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Rejected',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Rejected',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Pending',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
     {
-      
       profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
       name: 'XYZ',
       email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
+      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
+    },
+    {
+      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
+      name: 'XYZ',
+      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
+      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
+    },
+    {
+      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
+      name: 'XYZ',
+      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
+      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
+    },
+    {
+      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
+      name: 'XYZ',
+      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
+      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
+    },
+    {
+      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
+      name: 'XYZ',
+      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
+      _id: 'fsdf/dfdgfg/f/gf/f/gf/'
+    },
+    {
+      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
+      name: 'XYZ',
+      email: 'DEMO@GMAIL.COM ',
+      status: 'Verified',
       _id: 'fsdf/dfdgfg/f/gf/f/gf/'
     },
 
@@ -84,9 +107,10 @@ const User = () => {
       <div className="heading sticky">
         <h1>All COUNSELLORS</h1>
         <div className="row">
-          <div className="col"><h4>PROFILE PIC</h4></div>
+          <div className="col"><h4>IMAGE</h4></div>
           <div className="col"><h4>NAME</h4></div>
           <div className="col"><h4>EMAIL</h4></div>
+          <div className="col"><h4>STATUS</h4></div>
           <div className="col"><h4>PROFILE LINK</h4></div>
         </div>
       </div>
@@ -99,6 +123,10 @@ const User = () => {
               </div>
               <div className='col'>{counsellor.name}</div>
               <div className='col'>{counsellor.email}</div>
+              <div className={`col ${counsellor.status === 'Rejected' ? 'red' :
+                counsellor.status === 'Verified' ? 'green' :
+                  counsellor.status === 'Pending' ? 'blue' : ''
+                }`}>{counsellor.status}</div>
               <div className='col'>
                 <Link to='/counsellors/counsellor-profile'>
                   <p>View Profile</p>
