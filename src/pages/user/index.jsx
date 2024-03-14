dc946a5cb7d0bd38c294d82128222266d1b2bf21
Nikhil mandia <nikhilmandia7@gmail.com>
2024-03-14 13:55:33 +0530

    asasd

--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,107 +1,63 @@
 import { Link } from "react-router-dom";
-import "./style.scss"
-import { useState } from 'react';
+import "./style.scss";
+import { useContext, useState } from "react";
+import axios from "axios";
+import config from "@/config";
+const { backend_url } = config;
 
 const User = () => {
-  const [users, setUsers] = useState([
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-    {
-      profile_pic: 'https://toppng.com/uploads/preview/cool-avatar-transparent-image-cool-boy-avatar-11562893383qsirclznyw.png',
-      name: 'XYZ',
-      email: 'DEMO@GMAIL.COM ',
-    },
-  ]);
+  const [users, setUsers] = useState([]);
+  const getUsers = async () => {
+    try {
+      const { data } = await axios.get(`${backend_url}/user/users-for-admin`);
+      setUsers(data);
+      console.log("Response data:", data);
+    } catch (error) {
+      console.error(" error:", error);
+    }
+  };
+  const viewUser = (id) => {};
+
+  getUsers();
 
   return (
     <div className="User-container">
       <div className="heading sticky">
         <div className="row">
-          <div className="col"><h4>PROFILE PIC</h4></div>
-          <div className="col"><h4>NAME</h4></div>
-          <div className="col"><h4>EMAIL</h4></div>
-          <div className="col"><h4>VIEW PROFILE</h4></div>
+          <div className="col">
+            <h4>PROFILE PIC</h4>
+          </div>
+          <div className="col">
+            <h4>NAME</h4>
+          </div>
+          <div className="col">
+            <h4>EMAIL</h4>
+          </div>
+          <div className="col">
+            <h4>VIEW PROFILE</h4>
+          </div>
         </div>
       </div>
-      <div className='user-table-container'>
+      <div className="user-table-container">
         <div className="table">
           {users.map((user, i) => (
-            <div className='row' key={i}>
+            <div className="row" key={i}>
               <div className="col">
                 <img src={user.profile_pic} alt="user avatar" />
               </div>
-              <div className='col'>{user.name}</div>
-              <div className='col'>{user.email}</div>
-              <div className='col'>
-              <Link to='/user/user-details'>
-
-               <p>View Profile</p>
-               </Link>
+              <div className="col">{user.name}</div>
+              <div className="col">{user.email}</div>
+              <div className="col">
+                <Link to={`/user/user-details/${user._id}`}>
+                  <p>View Profile</p>
+                </Link>
               </div>
             </div>
           ))}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default User
+export default User;
