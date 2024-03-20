20ad6622d8dd5193da5146417734c6a765fbe226
khanNameera17 <92721716+khanNameera17@users.noreply.github.com>
2024-03-20 12:25:41 +0530

    assd

--- src/context/DashboardContext.js
+++ src/context/DashboardContext.js
@@ -1,49 +1,58 @@
 import axios from "axios";
-import { createContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import config from '@/config';
+import { AdminContext } from "./AdminContext";
+const { backend_url } = config;
 
 export const DashboardContext = createContext();
 
 export const DashboardProvider = ({ children }) => {
-  // const { admin } = useContext(AdminContext);
-  const [followersCount, setFollowersCount] = useState(0);
+  const { admin } = useContext(AdminContext);
+  const [dashboardData, setDashboardData] = useState({});
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const getFollowersCount = async () => {
-  //     try {
-  //       if (admin && admin.token) {
-  //         const response = await axios.get(
-  //           `${backend_url}/counsellor/follower/followers-count`,
-  //           {
-  //             headers: {
-  //               Authorization: admin.token
-  //             }
-  //           }
-  //         );
-  //         setFollowersCount(response.data.totalFollowers);
-  //       } else {
-  //         setFollowersCount(0); // Set followers count to 0 if admin or token is not available
-  //       }
-  //       setLoading(false); // Update loading state
-  //       setError(null); // Reset error state on successful response
-  //     } catch (error) {
-  //       setError(error.message); // Set error state with the error message
-  //       setLoading(false); // Update loading state
-  //       setFollowersCount(0); // Reset followers count on error
-  //     }
-  //   };
+  useEffect(() => {
+    const getDashboardData = async () => {
+      try {
+        if (admin && admin.token) {
+          const { data } = await axios.get(
+            `${backend_url}/admin/dashboard/dashboard-data`,
+            {
+              headers: {
+                Authorization: admin.token
+              }
+            }
+          );
+          setDashboardData(data);
+        } else {
+          setDashboardData({
+           totalUser: 0,
+           totalCounsellor: 0
+          }); // Set followers count to 0 if admin or token is not available
+        }
+        setLoading(false); // Update loading state
+        setError(null); // Reset error state on successful response
+      } catch (error) {
+        setError(error.message); // Set error state with the error message
+        setLoading(false); // Update loading state
+        setDashboardData({
+         totalUser: 0,
+          totalCounsellor: 0
+        });
+      }
+    };
 
-  //   getFollowersCount();
-  // }, [admin]); // Run the effect whenever the admin changes
+    getDashboardData();
+  }, [admin]); // Run the effect whenever the admin changes
 
   return (
     <DashboardContext.Provider
       value={{
-        // followersCount,
-        // setFollowersCount,
+        dashboardData,
+        setDashboardData,
         error,
-        loading,
+        loading
       }}
     >
       {children}
--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,73 +1,80 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import RecentPayments from "../../components/dashboardComponents/RecentPayments";
 import Widget from "../../components/dashboardComponents/widget";
 import "./style.scss";
 import BarChart from "../../components/dashboardComponents/BarChart";
-import { CounsellorData, UserData, EarningsData, ActivityData } from '../../components/dashboardComponents/Data';
+import {
+  CounsellorData,
+  UserData,
+  EarningsData,
+  ActivityData,
+} from "../../components/dashboardComponents/Data";
 import LineChart from "../../components/dashboardComponents/lineChart";
 import GroupedBarChart from "../../components/dashboardComponents/groupedBarChart";
+import { DashboardContext } from "../../context/DashboardContext";
 
 const Dashboard = () => {
+  const { dashboardData } = useContext(DashboardContext);
   const [userData, setUserData] = useState({
     labels: UserData.map((el) => el.month),
     datasets: [
       {
         label: "Users Gained",
-        data: UserData.map(el => el.userGain),
+        data: UserData.map((el) => el.userGain),
       },
       {
         label: "Users Lost",
-        data: UserData.map(el => el.userLost),
+        data: UserData.map((el) => el.userLost),
       },
-
-    ]
-  })
+    ],
+  });
   const [counsellorData, setCounsellorData] = useState({
     labels: CounsellorData.map((el) => el.month),
     datasets: [
       {
         label: "Counsellors Gained",
-        data: CounsellorData.map(el => el.counsellorGain),
+        data: CounsellorData.map((el) => el.counsellorGain),
       },
       {
         label: "Counsellors Lost",
-        data: CounsellorData.map(el => el.counsellorLost),
+        data: CounsellorData.map((el) => el.counsellorLost),
       },
-
-    ]
-  })
+    ],
+  });
   const [earningsData, setEarningsData] = useState({
     labels: EarningsData.map((el) => el.month),
     datasets: [
       {
         label: "Earnings Gained",
-        data: EarningsData.map(el => el.earning),
+        data: EarningsData.map((el) => el.earning),
       },
-    ]
-  })
+    ],
+  });
   const [activityData, setActivityData] = useState({
     labels: ActivityData.map((el) => el.day),
     datasets: [
       {
         label: "Online Users",
-        data: ActivityData.map(el => el.onlineUsers),
+        data: ActivityData.map((el) => el.onlineUsers),
       },
       {
         label: "Online Counsellors",
-        data: ActivityData.map(el => el.onlineCounsellors),
+        data: ActivityData.map((el) => el.onlineCounsellors),
       },
-    ]
-  })
+    ],
+  });
   return (
     <div className="all-dashboard">
       <div className="Dashboard-container">
         <div className="business-dashbaord">
           <h1>Business Dashboard</h1>
-           <div className="widgets-container"> 
+          <div className="widgets-container">
             <Widget heading="INCOME" value="$10000" />
-            <Widget heading="USERS" value="1000" />
-            <Widget heading="COUNSELLORS" value="1000" /> 
-           </div> 
+            <Widget heading="USERS" value={dashboardData.totalUser} />
+            <Widget
+              heading="COUNSELLORS" value={dashboardData.totalCounsellor}
+            />
+          </div>
           <div className="chart-row">
             <div className="col">
               <LineChart data={earningsData} />
@@ -77,8 +84,8 @@ const Dashboard = () => {
             </div>
           </div>
           <div className="chart-row">
-            <BarChart data={userData} text='Users' />
-            <BarChart data={counsellorData} text='Counsellors' />
+            <BarChart data={userData} text="Users" />
+            <BarChart data={counsellorData} text="Counsellors" />
           </div>
         </div>
 
