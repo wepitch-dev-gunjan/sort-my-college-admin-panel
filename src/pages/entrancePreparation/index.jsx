5b28d4824c9376c1e86f119f555410aa315ea836
khanNameera17 <92721716+khanNameera17@users.noreply.github.com>
2024-03-28 14:07:08 +0530

    epmain

--- src/pages/entrancePreparation/index.jsx
+++ src/pages/entrancePreparation/index.jsx
@@ -1,24 +1,44 @@
-import './style.scss'
-import {Link} from "react-router-dom";
-
+import "./style.scss";
+import { Link } from "react-router-dom";
+import card1 from "../../assets/card1.jpg";
+import card2 from "../../assets/card2.jpg";
 const EntrancePreparation = () => {
-    return (
-        <div className='admin-ep-main'>
-            <div className="admin-ep-child">
-                <div className="aepc-left">
-                    <Link className="aepc-links" to="/entrance-preparation/feature-and-preference">
-                        <h1> Feature & Preference Editor</h1>
-                    </Link>
-                    
-                </div>
-                <div className="aepc-right">
-                    <Link className="aepc-links" to="/entrance-preparation/institute-directory">
-                        <h1>Institute Directory</h1>
-                    </Link>
-                </div>
-            </div>
+  return (
+    <div className="admin-ep-main">
+      <div className="admin-ep-child">
+        {/* card 1 */}
+        <div className="card1">
+          <div className="aepc-left">
+            <img
+              src={card1}
+              alt="Card 1"
+            />
+          </div>
+          <Link
+            className="aepc-links"
+            to="/entrance-preparation/feature-and-preference"
+          >
+            <h1> Feature & Preference Editor</h1>
+          </Link>
         </div>
-    )
-}
+        {/* card 2  */}
+        <div className="card2">
+          <div className="aepc-right">
+            <img
+              src={card2}
+              alt="Card 2"
+            />
+          </div>
+          <Link
+            className="aepc-links"
+            to="/entrance-preparation/institute-directory"
+          >
+            <h1>Institute Directory</h1>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default EntrancePreparation
+export default EntrancePreparation;
