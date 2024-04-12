8cc18f59056e8b339fdf4d32e18c0a35b8df27d2
gunjan <gunjan.soral@gmail.com>
2024-04-12 17:31:48 +0530

    asdfasdfweru

--- src/config.js
+++ src/config.js
@@ -2,6 +2,8 @@ let configPromise;
 
 if (process.env.NODE_ENV === 'production') {
   configPromise = import('./config.prod.json').then(module => module.default);
+} else if (process.env.NODE_ENV === 'staging') {
+  configPromise = import('./config.stage.json').then(module => module.default);
 } else {
   configPromise = import('./config.dev.json').then(module => module.default);
 }
